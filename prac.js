873798f5d0ca2639c889ca0f1ab9d94a371d40f1
Ryosuke Iwadate <jaga0228@gmail.com>
2016-07-03 14:51:09 +0900

    Practice now

---
+++ prac.js
@@ -0,0 +1,29 @@
+// http://www.pori2.net/js/index.html
+
+//dialog message
+alert("test \nmessage");
+alert(2+3);
+
+//body message
+document.write("test<br>");
+document.write("おはようございます！！！！！！！！！！！！！！！！");
+
+//variable
+var a;
+var b, c, d;
+var a = 10;
+var strings = "つよし";
+alert(a);
+//can dock num and string in javascript
+alert(a + strings);
+
+//constant
+const A = "定数";
+alert(A);
+
+
+
+
+
+
+
