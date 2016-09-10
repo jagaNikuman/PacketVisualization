d638528457c8dac6f904b06cf06b2be1a1fa3160
Ryosuke Iwadate <jaga0228@gmail.com>
2016-09-11 01:44:22 +0900

    add mouse control, adjust boxOffsets

--- dotinstall.js
+++ dotinstall.js
@@ -2,8 +2,10 @@ var scene;
 var box;
 var camera;
 var renderer;
-var width = 600;
-var height = 600;
+// var width = 600;
+var width = window.parent.screen.width;
+// var height = 600;
+var height = window.parent.screen.height;
 
 
 //scene
@@ -14,47 +16,86 @@ scene = new THREE.Scene();
 // -geometry
 // -material
 var boxSize = 5;
-box = new THREE.Mesh(
+var boxSideOffset = 5;
+var boxHeightOffset = 5;
+box1 = new THREE.Mesh(
 	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
 	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
 );
-box.position.set(0,5,0);
-scene.add(box);
+box1.position.set(-boxSideOffset*9,boxHeightOffset,0);
+scene.add(box1);
 
 box2 = new THREE.Mesh(
-	new THREE.BoxGeometry(5,5,5),
-	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: true})
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
+	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
 );
-box2.position.set(10,5,0);
+box2.position.set(-boxSideOffset*7,boxHeightOffset,0);
 scene.add(box2);
 
 box3 = new THREE.Mesh(
-	new THREE.BoxGeometry(5,5,5),
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
 	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
 );
-box3.position.set(20,5,0);
+box3.position.set(-boxSideOffset*5,boxHeightOffset,0);
 scene.add(box3);
 
 box4 = new THREE.Mesh(
-	new THREE.BoxGeometry(5,5,5),
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
 	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
 );
-box4.position.set(30,5,0);
+box4.position.set(-boxSideOffset*3,boxHeightOffset,0);
 scene.add(box4);
 
 box5 = new THREE.Mesh(
-	new THREE.BoxGeometry(5,5,5),
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
 	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
 );
-box5.position.set(40,5,0);
+box5.position.set(-boxSideOffset*1,boxHeightOffset,0);
 scene.add(box5);
+
+box6 = new THREE.Mesh(
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
+	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
+);
+box6.position.set(boxSideOffset*1,boxHeightOffset,0);
+scene.add(box6);
+
+box7 = new THREE.Mesh(
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
+	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
+);
+box7.position.set(boxSideOffset*3,boxHeightOffset,0);
+scene.add(box7);
+
+box8 = new THREE.Mesh(
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
+	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
+);
+box8.position.set(boxSideOffset*5,boxHeightOffset,0);
+scene.add(box8);
+
+box9 = new THREE.Mesh(
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
+	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
+);
+box9.position.set(boxSideOffset*7,boxHeightOffset,0);
+scene.add(box9);
+
+box10 = new THREE.Mesh(
+	new THREE.BoxGeometry(boxSize,boxSize, boxSize),
+	new THREE.MeshLambertMaterial({color: 0x9aa1ac, wireframe: false})
+);
+box10.position.set(boxSideOffset*9,boxHeightOffset,0);
+scene.add(box10);
+
+
 
 //sphere
 var sphere = new THREE.Mesh(new THREE.SphereGeometry(5), new THREE.MeshPhongMaterial({color: 0xffffff}));
 sphere.castShadow = true;
 sphere.receiveShadow = true;
 scene.add(sphere);
-sphere.position.set(0,20,0);
+sphere.position.set(0,30,-30);
 
 
 //line 
@@ -67,6 +108,37 @@ line = new THREE.Line(lineGeometry, material);
 scene.add(line);
 
 
+// dynamic line
+var dynamicLinePoints = 30;
+var geometry = new THREE.BufferGeometry();
+var positions = new Float32Array(dynamicLinePoints * 32);
+geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
+var array = geometry.attributes.position.array;
+array[0]=0;
+array[1]=0;
+array[2]=0;
+array[3]=-10;
+array[4]=0;
+array[5]=0;
+array[6]=-20;
+array[7]=0;
+array[8]=0;
+array[9]=-30;
+array[10]=0;
+array[11]=0;
+array[12]=-40;
+array[13]=0;
+array[14]=0;
+geometry.addGroup(0, 2, 0);
+
+var material = new THREE.LineBasicMaterial({color: 0x99000, linewidth: 5});
+var line = new THREE.Line(geometry, material);
+
+scene.add(line);
+
+
+
+
 //plane
 var plane = new THREE.Mesh(
 	new THREE.PlaneGeometry(500, 500),
@@ -76,10 +148,15 @@ plane.rotation.x = -0.5* Math.PI;
 scene.add(plane);
 
 
+
 //camera
 camera = new THREE.PerspectiveCamera(45, width/height, 1, 1000);
-camera.position.set(80, 50, 100);
+camera.position.set(10, 50, 100);
 camera.lookAt(scene.position);
+
+
+//mouse contol
+var controls = new THREE.OrbitControls(camera);
 
 
 //light
@@ -103,42 +180,25 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 document.getElementById('stage').appendChild(renderer.domElement);
 
-//Move Camera from key input
-document.onkeydown = function (e) {
-	var key_code = e.keyCode;
-	console.log(key_code);
-	if(key_code == 37) {
-		// ←
-		camera.position.x -= 1;
-	}else if (key_code == 38) {
-		// ↑
-		camera.position.y += 1;
-	}else if (key_code == 39) {
-		// →
-		camera.position.x += 1;
-	}else if (key_code == 40) {
-		// ↓
-		camera.position.y -= 1;
-	}else if (key_code == 34) {
-		// PageDown
-		// camera.position.z -= 1;
-	}else if (key_code == 35) {
-		// End
-		camera.position.z -= 1;
-	}else if (key_code == 36) {
-		// Home
-		camera.position.z += (1/Math.PI);
-	}else if (key_code == 33) {
-		// PageUP
-		// camera.rotation.z += (1/Math.PI);
-	}
 
-	camera.lookAt(scene.position);
-}
+function Sleep( T ){ 
+   var d1 = new Date().getTime(); 
+   var d2 = new Date().getTime(); 
+   while( d2 < d1+1000*T ){    //T秒待つ 
+       d2=new Date().getTime(); 
+   } 
+   return; 
+} 
+//1秒待つ 
 
 //Rendering Loop
+var i = 1;
 function loop() {
 	requestAnimationFrame(loop);
+	line.geometry.setDrawRange(0,i);
+	i++;
+	// Sleep( 1 );
+	controls.update();
 	renderer.render(scene, camera);
 }
 loop();
