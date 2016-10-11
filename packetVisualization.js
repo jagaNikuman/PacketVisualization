4a7dda75709f7d034e8fa4cd0f2b885e344154ba
Ryosuke Iwadate <jaga0228@gmail.com>
2016-10-12 04:02:19 +0900

    fix right side lines, add motion

--- packetVisualization.js
+++ packetVisualization.js
@@ -17,7 +17,7 @@ var boxSize = 5;
 var boxSideOffset = 5;
 var boxHeightOffset = 5;
 var boxArray = Array();
-var boxNum = 10;
+var boxNum = 30;
 for(var i = 0; i < boxNum; i++) {
 	var box = new THREE.Mesh(
 		new THREE.BoxGeometry(boxSize, boxSize, boxSize),
@@ -43,7 +43,7 @@ sphere.position.set(0,30,-30);
 var dynamicLineArray = new Array();
 var dynamicLinePointsArray = new Array();
 
-for(var num = 0; num < 10; num++) {
+for(var num = 0; num < boxNum; num++) {
 	function drawDynamicLine() {
 		dynamicLinePointsArray[num] = 100;
 		var dynamicLineGeometry = new THREE.BufferGeometry();
@@ -91,27 +91,49 @@ for(var num = 0; num < 10; num++) {
 		}
 
 		console.log("widthArray");
-		var dynamicLineWidthMin = boxArray[num].position.x + boxSize/2;
-		var dynamicLineWidthMax = sphere.position.x;
-		var dynamicLineWidthLength = dynamicLineWidthMax - dynamicLineWidthMin;
-		dynamicLineLength += dynamicLineWidthLength;
-		dynamicLineCounter = 0;
-		console.log(dynamicLineWidthLength);
-		var i;
-		for(i=(dynamicLineLength - dynamicLineWidthLength)*3 +6; i < dynamicLineLength*3 +6; i = i+3) {
-			if(num < boxNum/2) {
+		if(num < boxNum/2) {
+			var dynamicLineWidthMin = boxArray[num].position.x + boxSize/2;
+			var dynamicLineWidthMax = sphere.position.x;
+			var dynamicLineWidthLength = dynamicLineWidthMax - dynamicLineWidthMin;
+			dynamicLineLength += dynamicLineWidthLength;
+			dynamicLineCounter = 0;
+			console.log(dynamicLineWidthLength);
+			var i;
+			for(i=(dynamicLineLength - dynamicLineWidthLength)*3 +6; i < dynamicLineLength*3 +6; i = i+3) {
+				if(num < boxNum/2) {
+					array[i-3] = dynamicLineWidthMin + dynamicLineLengthOffset*dynamicLineCounter;		//x
+					array[i-2] = dynamicLineHeightMax;		//y
+					array[i-1] = dynamicLineDepthMin;		//z
+				}else{
+					array[i-3] = boxArray[num].position.x;		//x
+					array[i-2] = dynamicLineHeightMin + dynamicLineLengthOffset*dynamicLineCounter;		//y
+					array[i-1] = 0;		//z
+				}
 				array[i-3] = dynamicLineWidthMin + dynamicLineLengthOffset*dynamicLineCounter;		//x
 				array[i-2] = dynamicLineHeightMax;		//y
 				array[i-1] = dynamicLineDepthMin;		//z
-			}else{
+				dynamicLineCounter++;
+				console.log("%d: Array[%d - %d] = {%d, %d, %d}",_debugCounter ,i-3, i-1, array[i-3], array[i-2], array[i-1]);
+				_debugCounter++;
+				// console.log(i);
+			}
+		}else{
+			var dynamicLineWidthMax = boxArray[num].position.x + boxSize/2;
+			var dynamicLineWidthMin = sphere.position.x;
+			var dynamicLineWidthLength = dynamicLineWidthMax - dynamicLineWidthMin;
+			dynamicLineLength += dynamicLineWidthLength;
+			dynamicLineCounter = 0;
+			console.log(dynamicLineWidthLength);
+			var i = 0;
+			for(i=(dynamicLineLength - dynamicLineWidthLength)*3 +6; i < dynamicLineLength*3 +6; i = i+3) {
 				array[i-3] = dynamicLineWidthMax - dynamicLineLengthOffset*dynamicLineCounter;		//x
 				array[i-2] = dynamicLineHeightMax;		//y
 				array[i-1] = dynamicLineDepthMin;		//z
+				dynamicLineCounter++;
+				console.log("%d: Array[%d - %d] = {%d, %d, %d}",_debugCounter ,i-3, i-1, array[i-3], array[i-2], array[i-1]);
+				_debugCounter++;
+				// console.log(i);
 			}
-			dynamicLineCounter++;
-			console.log("%d: Array[%d - %d] = {%d, %d, %d}",_debugCounter ,i-3, i-1, array[i-3], array[i-2], array[i-1]);
-			_debugCounter++;
-			// console.log(i);
 		}
 		dynamicLinePointsArray[num] = i/3;
 		dynamicLineGeometry.addGroup(0, 2, 0);
@@ -166,14 +188,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 document.getElementById('stage').appendChild(renderer.domElement);
 
-// var drawLine1Flag = false;
-// var drawLine2Flag = false;
-// var drawLine3Flag = false;
-// var drawLine4Flag = false;
-// var drawLine5Flag = false;
-// var drawLine6Flag = false;
-// var drawLine7Flag = false;
-// var drawLine8Flag = false;
+
 // //Debug dynamicLine by Keyboard
 // /*
 // 1: 49
@@ -187,54 +202,58 @@ document.getElementById('stage').appendChild(renderer.domElement);
 // 9: 57
 // 0: 48
 // */
-// document.onkeydown = function (e) {
-// 	var key_code = e.keyCode;
-// 	console.log(key_code);
-// 	if(key_code == 49) {
-// 		console.log("draw box1 line");
-// 		drawLine1Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 50) {
-// 		console.log("draw box2 line");
-// 		drawLine2Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 51) {
-// 		console.log("draw box3 line");
-// 		drawLine3Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 52) {
-// 		console.log("draw box4 line");
-// 		drawLine4Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 53) {
-// 		console.log("draw box5 line");
-// 		drawLine5Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 54) {
-// 		console.log("draw box6 line");
-// 		drawLine6Flag = true;
-// 		console.log(drawLine1Flag);
-// 		drawLine6Flag = true;
-// 	}else if (key_code == 55) {
-// 		console.log("draw box7 line");
-// 		drawLine7Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 56) {
-// 		console.log("draw box8 line");
-// 		drawLine8Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 57) {
-// 		console.log("draw box9 line");
-// 		drawLine9Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}else if (key_code == 48) {
-// 		console.log("draw box10 line");
-// 		drawLine10Flag = true;
-// 		console.log(drawLine1Flag);
-// 	}
+var drawLineFragArray = new Array();
+for(var i = 0; i < boxNum; i++) {
+	drawLineFragArray[i] = false;
+}
 
-// 	camera.lookAt(scene.position);
-// }
+document.onkeydown = function (e) {
+	var key_code = e.keyCode;
+	console.log(key_code);
+	if(key_code == 49) {
+		console.log("draw box0 line");
+		drawLineFragArray[0] = true;
+		console.log(drawLineFragArray[0]);
+	}else if (key_code == 50) {
+		console.log("draw box1 line");
+		drawLineFragArray[1] = true;
+		console.log(drawLineFragArray[1]);
+	}else if (key_code == 51) {
+		console.log("draw box2 line");
+		drawLineFragArray[2] = true;
+		console.log(drawLineFragArray[2]);
+	}else if (key_code == 52) {
+		console.log("draw box3 line");
+		drawLineFragArray[3] = true;
+		console.log(drawLineFragArray[3]);
+	}else if (key_code == 53) {
+		console.log("draw box4 line");
+		drawLineFragArray[4] = true;
+		console.log(drawLineFragArray[4]);
+	}else if (key_code == 54) {
+		console.log("draw box5 line");
+		drawLineFragArray[5] = true;
+		console.log(drawLineFragArray[5]);
+	}else if (key_code == 55) {
+		console.log("draw box6 line");
+		drawLineFragArray[6] = true;
+		console.log(drawLineFragArray[6]);
+	}else if (key_code == 56) {
+		console.log("draw box7 line");
+		drawLineFragArray[7] = true;
+		console.log(drawLineFragArray[7]);
+	}else if (key_code == 57) {
+		console.log("draw box8 line");
+		drawLineFragArray[8] = true;
+		console.log(drawLineFragArray[8]);
+	}else if (key_code == 48) {
+		console.log("draw box9 line");
+		drawLineFragArray[9] = true;
+		console.log(drawLineFragArray[9]);
+	}
+
+	camera.lookAt(scene.position);
+}
 
 
 function Sleep( T ){ 
@@ -245,114 +264,29 @@ function Sleep( T ){
    } 
    return; 
 } 
-//1秒待つ 
 
 //Rendering Loop
 var i = 0;
-var dLine1cnt = 0;
-var dLine2cnt = 0;
-var dLine3cnt = 0;
-var dLine4cnt = 0;
-var dLine5cnt = 0;
-var dLine6cnt = 0;
-var dLine7cnt = 0;
-var dLine8cnt = 0;
+var drawLineCountArray = new Array();
+for(var i = 0; i < boxNum; i++) {
+	drawLineCountArray[i] = 0;
+}
+
+var cnt = 0;
 function loop() {
 	requestAnimationFrame(loop);
-	// console.log(drawLine1Flag);
-	// // console.log(i);
-
-	// if(dLine1cnt+10 < dynamicLinePoints && drawLine1Flag == true) {
-	// 	dynamicLine.geometry.setDrawRange(dLine1cnt,10);
-	// 	dLine1cnt++;
-	// }else{
-	// 	dynamicLine.geometry.setDrawRange(0,1);
-	// 	dLine1cnt = 0;
-	// 	drawLine1Flag = false;
-	// 	console.log(dynamicLinePoints);
-	// }
+	for(var i = 0; i < boxNum; i++) {
+		if(drawLineCountArray[i]+10 < dynamicLinePointsArray[i] && drawLineFragArray[i] == true) {
+			dynamicLineArray[i].geometry.setDrawRange(drawLineCountArray[i], 10);
+			drawLineCountArray[i]++;
+		}else{
+			dynamicLineArray[i].geometry.setDrawRange(0, 1);
+			drawLineCountArray[i] = 0;
+			drawLineFragArray[i] = true;
+		}
+	}
 	
-	// if(dLine2cnt+10 < dynamicLine2Points && drawLine2Flag == true) {
-	// 	dynamicLine2.geometry.setDrawRange(dLine2cnt,10);
-	// 	dLine2cnt++;
-	// }else{
-	// 	dynamicLine2.geometry.setDrawRange(0,1);
-	// 	dLine2cnt = 0;
-	// 	drawLine2Flag = false;
-	// }
-	
-	// if(dLine3cnt+10 < dynamicLine3Points && drawLine3Flag == true) {
-	// 	dynamicLine3.geometry.setDrawRange(dLine3cnt,10);
-	// 	dLine3cnt++;
-	// }else{
-	// 	dynamicLine3.geometry.setDrawRange(0,1);
-	// 	dLine3cnt = 0;
-	// 	drawLine3Flag = false;
-	// }
-	
-	// if(dLine4cnt+10 < dynamicLine4Points && drawLine4Flag == true) {
-	// 	dynamicLine4.geometry.setDrawRange(dLine4cnt,10);
-	// 	dLine4cnt++;
-	// }else{
-	// 	dynamicLine4.geometry.setDrawRange(0,1);
-	// 	dLine4cnt = 0;
-	// 	drawLine4Flag = false;
-	// }
-	
-	// if(dLine5cnt+10 < dynamicLine5Points && drawLine5Flag == true) {
-	// 	dynamicLine5.geometry.setDrawRange(dLine5cnt,10);
-	// 	dLine5cnt++;
-	// }else{
-	// 	dynamicLine5.geometry.setDrawRange(0,1);
-	// 	dLine5cnt = 0;
-	// 	drawLine5Flag = false;
-	// }
-		
-	// if(dLine6cnt+10 < dynamicLine6Points && drawLine6Flag == true) {
-	// 	dynamicLine6.geometry.setDrawRange(dLine6cnt,10);
-	// 	dLine6cnt++;
-	// }else{
-	// 	dynamicLine6.geometry.setDrawRange(0,1);
-	// 	dLine6cnt = 0;
-	// 	drawLine6Flag = false;
-	// }
-		
-	// if(dLine7cnt+10 < dynamicLine7Points && drawLine7Flag == true) {
-	// 	dynamicLine7.geometry.setDrawRange(dLine7cnt,10);
-	// 	dLine7cnt++;
-	// }else{
-	// 	dynamicLine7.geometry.setDrawRange(0,1);
-	// 	dLine7cnt = 0;
-	// 	drawLine7Flag = false;
-	// }
-		
-	// if(dLine8cnt+10 < dynamicLine8Points && drawLine8Flag == true) {
-	// 	dynamicLine8.geometry.setDrawRange(dLine8cnt,10);
-	// 	dLine8cnt++;
-	// }else{
-	// 	dynamicLine8.geometry.setDrawRange(0,1);
-	// 	dLine8cnt = 0;
-	// 	drawLine8Flag = false;
-	// }
-	
-	// if(dLine6cnt+10 < dynamicLine6Points && drawLine6Flag == true) {
-	// 	dynamicLine6.geometry.setDrawRange(dLine6cnt,10);
-	// 	dLine6cnt++;
-	// }else{
-	// 	dynamicLine6.geometry.setDrawRange(0,1);
-	// 	dLine6cnt = 0;
-	// 	drawLine6Flag = false;
-	// }
-	
-	// if(dLine6cnt+10 < dynamicLine6Points && drawLine6Flag == true) {
-	// 	dynamicLine6.geometry.setDrawRange(dLine6cnt,10);
-	// 	dLine6cnt++;
-	// }else{
-	// 	dynamicLine6.geometry.setDrawRange(0,1);
-	// 	dLine6cnt = 0;
-	// 	drawLine6Flag = false;
-	// }
-	
+	//周期調整
 	// Sleep( 0.05 );
 	controls.update();
 	renderer.render(scene, camera);
